97a9c7334b352a79f39662c70cddbf6aafe79c94
kaveena <kaveena.persand@gmail.com>
2015-11-17 08:05:32 +0100

    Update main.c

--- projet_bateau/src/main.c
+++ projet_bateau/src/main.c
@@ -3,69 +3,154 @@
 // ETAT : 
 //__________________________________________________________
 
-// registres de périphériques du stm32
+// registres de pÃ©riphÃ©riques du stm32
 #include "stm32f10x.h"
 #include "clock.h"
 #include "adc.h"
 #include "gpio.h"
 #include "timer1234.h"
+#include "math.h"
+#include "string.h"
 
-void notreTIM4_IRQHandler(void)
-{
-	float duty_cycle_moteur;
-	float tmp_impulsion;
-	//detecter CC2E comme IT
-	if((TIM4->SR & TIM_SR_CC2IF) != 0)
-	{
-		/* Calcul de la durÃ©e de l'impulsion */
-		/* Une durÃ©e de 1.5ms correspond Ã  une commande de 0% */
-		/* Une deviation maximum de 5ms autour de 1.5ms est permise */
-		/* 2ms => 100% sens '0' */
-		/* 1ms => 100% sens '1' */
-		tmp_impulsion = CLOCK_GetTIMCLK(TIM4);
-		tmp_impulsion = (1/tmp_impulsion)*((TIM4->PSC)+1)*((TIM4->CCR2)+1);
-		tmp_impulsion -= 0.0015;
-		duty_cycle_moteur = tmp_impulsion * 100 / 0.0005;
-		
-		//implÃ©mentation d'une zone morte de 5% autour de 1.5ms 
-		if(duty_cycle_moteur > 5)
-			/*sens '0'*/
-		{
-			Port_IO_Reset(GPIOA, 2);
-		}
-		else if(duty_cycle_moteur < -5)
-		{
-			/*sens '1'*/
-			duty_cycle_moteur = -duty_cycle_moteur;
-			Port_IO_Set(GPIOA, 2);
-		}
-		else
-		{
-				duty_cycle_moteur = 0;
-		}
-		if(duty_cycle_moteur>100)
-			/*prise en compte d'une saturation*/
-			{duty_cycle_moteur = 100;} 
-		charger_DC_pwm(TIM2,2,duty_cycle_moteur);
+#define Y_AXIS_CHANNEL 11
+#define Y_AXIS_1g 2.13
+#define Y_AXIS_sens 0
+#define ACCELERO_Sensitivity 0.48
+
+
+#define BATTERY_CHANNEL 12
+#define VCC 12
+#define VCC_ratio 13
+#define ADC_VDD 3.3
+
+#define PI 3.14159265
+
+#define LONGUEUR_MAX 50
+
+/* Si le sens est Ã©gal Ã  0 plus le bateau */
+/* s'incline plus et moins l'acceleration  */
+/* sera positive*/
+
+		double angle;
+		u16 batterie;
+		u8 alerte_roulis;
+		u8 alerte_batterie;
+
+
+	void send_UART( char message, USART_TypeDef * USART){
+		while(USART->SR && USART_SR_TC == 0);
+		USART->DR = message;	
+		USART->CR1 |= USART_CR1_TE;
 	}
-	TIM4->SR = 0;
-}
+	
+	void function_UART(u8 alerte_roulis, u8 alerte_batterie){
+		char message[LONGUEUR_MAX];
+		/*
+		char message1[] ="ANGLE ROULIS TROP GRAND! & BATTERIE FAIBLE!";
+		char message2[] ="ANGLE ROULIS TROP GRAND";
+		char message3[] ="BATTERIE FAIBLE!";
+		char message4[] ="OKAY";
+		*/
+		
+		
+		//messages de test
+		char message1[] ="AA";
+		char message2[] ="BB";
+		char message3[] ="CC";
+		char message4[] ="DD";
+		
+		int i = 0;
+		char  char_msg;
+		if ((alerte_roulis==1)&(alerte_batterie==1)){
+			strcpy(message,message1);
+		}
+		else if ((alerte_roulis==1)&(alerte_batterie==0)){
+			strcpy(message,message2);
+		}
+		else if ((alerte_roulis==0)&(alerte_batterie==1)){
+			strcpy(message, message3 );
+		}
+		else{
+			strcpy(message, message4);
+		}
+		char_msg = message[0];
+		
+		while (char_msg != '\0'){
+			send_UART(*message, USART1);
+			i++;
+			char_msg=message[i];
+		}
+	}
 
 
+  void ADC_IT (void){			
+		u16 y;
 
+		if ((ADC1->SR & ADC_SR_EOC) ==ADC_SR_EOC){
+			y = (ADC1 -> DR) &~ (0xF000);
+			ADC1->SR = ADC1->SR &~ ADC_SR_EOC;
+		}
+		if ((ADC2->SR & ADC_SR_EOC) ==ADC_SR_EOC){
+			batterie = (ADC2->DR)&~ (0xF000);
+			ADC2->SR = ADC2->SR &~ ADC_SR_EOC;
+		}
+		angle = acos(((y*ADC_VDD/0xFFF)-(Y_AXIS_1g-ACCELERO_Sensitivity))/ACCELERO_Sensitivity)*180/PI;
+		
+		if ((angle>45)&(alerte_roulis==0))
+			alerte_roulis=1;
+		else if((angle<44)&&(alerte_roulis==1))
+			alerte_roulis=0;
+		if ((batterie*ADC_VDD/0xFFF)<(VCC*0.8/VCC_ratio))
+			alerte_batterie=1;
+		else if ((batterie*ADC_VDD/0xFFF)>(VCC*0.81/VCC_ratio))
+			alerte_batterie=0;
+		function_UART(alerte_roulis, alerte_batterie);
+ }
+
+ 
+ void Timer_IT(void){
+	 TIM1->SR = TIM1->SR &~ TIM_SR_UIF;
+	 start_conv(ADC1);
+	 start_conv(ADC2);
+ }
+ 
+
+	 void Config_UART_Projet_Bateau(USART_TypeDef * USART){
+		 RCC -> APB2ENR |= RCC_APB2ENR_USART1EN;
+		//configuration UART
+		USART1->CR1 |= USART_CR1_UE;
+		//BRR mantisse 468 et fraction 75 = 12/16
+		USART1->BRR = (468<<4) + 12;
+	 }
+ 
 int main (void) {
+
 	CLOCK_Configure();
-	Timer_Init_PWM_Input(TIM4, 1, 25);
-	Port_IO_Init_AF_Output( GPIOA, 1);
-	Port_IO_Init_Output( GPIOA, 2);
-	Port_IO_Init_Input( GPIOB, 6);
-	
-	Timer_Active_IT(TIM4,5, 2,notreTIM4_IRQHandler);
-	
-	Timer_1234_Init(TIM2, 50);
-	config_pwm(TIM2, 0, 2, 40);
+
+	Port_IO_Init_AF_Output (GPIOA, 9); //USART : liaison HF
+	alerte_roulis=0;
+	alerte_batterie=0;
 
 	
+	Config_UART_Projet_Bateau(USART1);
+	Timer_1234_Init(TIM1, 20000);
+	Timer_Init_Compare (TIM1, 50.0, 2);
+	power_ADC(ADC1);
+	power_ADC(ADC2);
+	config_adc_single_channel(ADC1, Y_AXIS_CHANNEL);
+	config_adc_single_channel(ADC2, BATTERY_CHANNEL);
+	Active_IT_ADC(ADC1, 15,  &ADC_IT);
+	Active_IT_ADC(ADC2, 15,  &ADC_IT);
+	Timer_Active_IT( TIM1, 17, 0, &Timer_IT);
+
+	
+	
+
+
+
+	
+
+
 	while(1);
-	return 0;
+
 }
