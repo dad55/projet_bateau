fd09301b200d135f7596d112d121ac9661a13e82
kaveena <kaveena.persand@gmail.com>
2015-11-21 18:20:24 +0100

    Decouplage couche périph et service

--- pilotes/adc/adc.h
+++ pilotes/adc/adc.h
@@ -18,12 +18,20 @@ u16 read_result_conv(ADC_TypeDef * ADC);
 void start_conv(ADC_TypeDef * ADC);
 
 
-/*Active l'intérruption ADC1 et ADC2     */
-/*Remarque ADC1 et ADC2 ont une seule    */
-/*intérruption globale et si on utilise  */
-/*cette fonction pour changer le handler */
-/*de l'un autre et automatiquemen modifié*/
+/*Active l'intérruption ADC1 et ADC2     
+  Remarque : ADC1 et ADC2 ont une seule    
+  intérruption globale et si on utilise  
+  cette fonction pour changer le handler 
+  de l'un autre et automatiquement modifié*/
 void Active_IT_ADC(ADC_TypeDef * ADC, u8 Priority,  void (*IT_function) (void));
+
+
+u16 lire_ADC(ADC_TypeDef * ADC);
+
+/* Fonction qui renvoit 0 si la conversion
+   a fini, une valeur different de 0 sinon
+*/
+u8 ADC_ready(ADC_TypeDef * ADC);
 
 #endif
 
